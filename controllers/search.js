b3e8dbf1bdd5c6c18b6164755bc4aca4b5b125ba
code-Name-ankit <jalandharaankit4@gmail.com>
2025-07-13 21:18:52 +0530

    Updated in search

--- controllers/search.js
+++ controllers/search.js
@@ -3,25 +3,20 @@ const Fuse = require("fuse.js");
 //Search Route
 module.exports.searchListings = async (req, res) => {
   const query = req.query.q || "";
-  const allListings = await Listing.find({});
+  const alllistings = await Listing.find({});
 
-  
-    const fuse = new Fuse(allListings, {
-      keys: ["title", "description", "location","country"],
-      threshold: 0.3,  // smaller = stricter match
-    });
+  const fuse = new Fuse(alllistings, {
+    keys: ["title", "description", "location", "country"],
+    threshold: 0.3, // smaller = stricter match
+  });
 
-    const result = fuse.search(query);
-   let  listings = result.map(r => r.item);
-  
+  const result = fuse.search(query);
+  let allListings = result.map((r) => r.item);
 
-
-if (!listings || listings.length === 0 ) {
+  if (!allListings || allListings.length === 0) {
     req.flash("error", " This Listing is not found");
     res.redirect("/listings");
   } else {
-    res.render("listings/results", { listings });
+        res.render("listings/index", {  allListings });
   }
-
-  
 };
