d8318bb7dff48caf6eef2c1da61d65b12aaae281
code-Name-ankit <jalandharaankit4@gmail.com>
2025-07-13 20:52:29 +0530

    Updated in search-simmiler word

--- controllers/search.js
+++ controllers/search.js
@@ -1,23 +1,26 @@
 const Listing = require("../models/listing.js");
-
+const Fuse = require("fuse.js");
 //Search Route
 module.exports.searchListings = async (req, res) => {
   const query = req.query.q || "";
-  const allListings = await Listing.find({
-    $or: [
-      { title: { $regex: query, $options: "i" } },
-      { description: { $regex: query, $options: "i" } },
-      { location: { $regex: query, $options: "i" } },
-      {country: { $regex: query, $options: "i" }}
-    ],
-  });
+  const allListings = await Listing.find({});
+
+  
+    const fuse = new Fuse(allListings, {
+      keys: ["title", "description", "location","country"],
+      threshold: 0.3,  // smaller = stricter match
+    });
+
+    const result = fuse.search(query);
+   let  listings = result.map(r => r.item);
+  
 
 
-if (!allListings || allListings.length === 0 ) {
+if (!listings || listings.length === 0 ) {
     req.flash("error", " This Listing is not found");
     res.redirect("/listings");
   } else {
-    res.render("listings/results", { allListings });
+    res.render("listings/results", { listings });
   }
 
   
